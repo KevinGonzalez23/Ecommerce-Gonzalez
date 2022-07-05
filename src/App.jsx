1b1595b4e83bae5903c008ebdfe1c080a5ada35c
Kevin Gonzalez <gonzalezhugokevin@gmail.com>
2022-07-05 13:22:49 -0300

    Ordenando todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx"
+
+
+const App = () => {
+    return (
+      <>
+        <Header/>
+        <ItemListContainer/>
+      </>  
+    )
+  }
+
+export default App
